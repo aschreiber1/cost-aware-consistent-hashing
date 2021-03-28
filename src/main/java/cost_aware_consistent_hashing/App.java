baadba3f94d6c6f0ebadbf8006d7f4b63b0f2ad2
aschreiber1 <aschreiber1@fordham.edu>
2021-03-28 09:14:02 -0400

    InitalCommit

---
+++ src/main/java/cost_aware_consistent_hashing/App.java
@@ -0,0 +1,13 @@
+package cost_aware_consistent_hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
